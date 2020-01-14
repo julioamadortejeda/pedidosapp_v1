7901b6171723811741b641c3a0127e26aa062aa4
julio cesar amador tejeda <julio.amadortejeda@gmail.com>
2020-01-13 22:34:34 -0600

    cambios en el dieño del home_page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -36,111 +36,135 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _renderCards(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Column(
       children: <Widget>[
-        _renderPedidoCard(context),
+        _renderAppName(context, size),
+        _renderPedidoCard(context, size),
         Expanded(child: Container()),
-        _renderCardMessage(context)
+        _renderCardMessage(context, size)
       ],
     );
   }
 
-  Widget _renderPedidoCard(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
+  Widget _renderAppName(BuildContext context, Size size) {
     return SafeArea(
       child: Container(
-        padding: EdgeInsets.only(left: 50.0, right: 50.0, top: 50.0),
-        //margin: EdgeInsets.only(bottom: 10.0),
-        height: size.height * 0.5,
-        child: Card(
-          elevation: 5.0,
-          //color: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-          child: InkWell(
-            splashColor: pedido.color.withAlpha(255),
-            onTap: () {},
-            child: Column(children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Text(
-                  'Pedido #${pedido.id}',
-                  style: TextStyle(
-                      fontFamily: 'Dosis',
-                      color: pedido.color,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w400),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 5.0),
-                child: Text(
-                  pedido.estatus,
-                  style: TextStyle(
-                      fontFamily: 'Dosis',
-                      color: pedido.color,
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 70.0, vertical: 20.0),
-                child: FadeInImage(
-                  width: double.infinity,
-                  image: AssetImage(pedido.image),
-                  placeholder: AssetImage('assets/images/loader.gif'),
-                  fadeInDuration: Duration(milliseconds: 200),
-                  fit: BoxFit.fill,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10.0),
-                child: Text(
-                  'Termina en: 3D 2H 60M 53S',
-                  style: TextStyle(
-                      fontFamily: 'Dosis',
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w400,
-                      color: pedido.color.withAlpha(200)),
-                ),
-              )
-            ]),
-          ),
+        margin: EdgeInsets.only(top: size.height * 0.05),
+        child: Text(
+          'PEDIDOS SHEIN',
+          style: TextStyle(
+              fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
     );
   }
 
-  Widget _renderCardMessage(BuildContext context) {
+  Widget _renderPedidoCard(BuildContext context, Size size) {
+    //final size = MediaQuery.of(context).size;
+    print(size.height);
+    return Container(
+      padding:
+          EdgeInsets.only(left: 50.0, right: 50.0, top: size.height * 0.05),
+      //color: Colors.red,
+      //margin: EdgeInsets.only(bottom: 10.0),
+      height: size.height * (size.height < 600 ? 0.65 : 0.60),
+      child: Card(
+        elevation: 5.0,
+        //color: Colors.white,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: InkWell(
+          splashColor: pedido.color.withAlpha(255),
+          onTap: () {},
+          child: Column(children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: size.height * 0.03),
+              child: Text(
+                'Pedido #${pedido.id}',
+                style: TextStyle(
+                    fontFamily: 'Dosis',
+                    color: pedido.color,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: size.height * 0.01),
+              child: Text(
+                pedido.estatus,
+                style: TextStyle(
+                    fontFamily: 'Dosis',
+                    color: pedido.color,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: 70.0, vertical: size.height * 0.02),
+              child: FadeInImage(
+                width: double.infinity,
+                image: AssetImage(pedido.image),
+                placeholder: AssetImage('assets/images/loader.gif'),
+                fadeInDuration: Duration(milliseconds: 200),
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: size.height * 0.01),
+              child: Text(
+                'Termina en:',
+                style: TextStyle(
+                    fontFamily: 'Dosis',
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w300,
+                    color: pedido.color),
+              ),
+            ),
+            //SizedBox(height: size.height * 0.01,),
+            Text(
+              '3D 2H 60M 53S',
+              style: TextStyle(
+                  fontFamily: 'Dosis',
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                  color: pedido.color.withAlpha(200)),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget _renderCardMessage(BuildContext context, Size size) {
     final size = MediaQuery.of(context).size;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-            //padding: EdgeInsets.only(left: 20.0),
-            child: Center(
-              child: Text(
-                'Mensajes',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-            )),
-        SizedBox(
-          height: 10.0,
+    return Container(
+      // padding: EdgeInsets.all(2.0),
+      //width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
+          color: Colors.white),
+      //padding: EdgeInsets.symmetric(horizontal: 50.0),
+      height: size.height * 0.1,
+      child: Material(
+        color: Colors.transparent,
+        borderOnForeground: false,
+        child: InkWell(
+          splashColor: pedido.color,
+          onTap: () {},
+          child: Center(
+            child: Text(
+              'Mensajes',
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: pedido.color,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
-        Container(
-          //width: double.infinity,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0), color: Colors.white),
-          //padding: EdgeInsets.symmetric(horizontal: 50.0),
-          height: size.height * 0.35,
-          child: _crearLista(context),
-        )
-      ],
+      ),
     );
   }
 
@@ -154,7 +178,8 @@ class HomePage extends StatelessWidget {
           child: ListTile(
             title: Text('Entry ${messages[index].title}'),
             subtitle: Text(messages[index].subTitle),
-            trailing: Icon(Icons.keyboard_arrow_right, color: messages[index].color),
+            trailing:
+                Icon(Icons.keyboard_arrow_right, color: messages[index].color),
             leading: ClipRRect(
                 borderRadius: BorderRadius.circular(10.0),
                 child: Image(
@@ -174,5 +199,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-
 }
